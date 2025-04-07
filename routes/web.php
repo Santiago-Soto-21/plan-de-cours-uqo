1d10994da2be0135eea49edeae94edf00b9608ed
Santiago-Soto-21 <santisotofajardo@gmail.com>
2025-04-07 18:15:25 -0400

    Add routes for PdfController.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PdfController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -35,5 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/generate-pdf', [PdfController::class, 'generate'])->name('generate.pdf');
 
 require __DIR__.'/auth.php';
