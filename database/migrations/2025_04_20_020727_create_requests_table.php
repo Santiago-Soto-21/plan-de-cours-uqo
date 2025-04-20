451d473cd1944f43c975fe69280f8506f67f6237
Santiago-Soto-21 <santisotofajardo@gmail.com>
2025-04-19 22:22:45 -0400

    Create table template for migration

---
+++ database/migrations/2025_04_20_020727_create_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('status')->default('En attente');
+            $table->string('filename');
+            $table->string('requestor_first_name');
+            $table->string('requestor_last_name');
+            $table->text('comment')->nullable();
+            $table->timestamps(); // This creates both created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
