b0d8f7e7a05ae6d42f03003d8f1a34016b185f4f
Santiago-Soto-21 <santisotofajardo@gmail.com>
2025-04-19 22:23:17 -0400

    Create model for interaction with backend

---
+++ app/Models/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'status',
+        'filename',
+        'requestor_first_name',
+        'requestor_last_name',
+        'comment',
+    ];
+}
