e2ec8f0d5c5efa27043248b7b3af6448cc49bbe5
Santiago-Soto-21 <santisotofajardo@gmail.com>
2025-04-21 20:22:21 -0400

    Change table columns to hold secretary and director comments

--- database/migrations/2025_04_21_230944_create_requests_table.php
+++ database/migrations/2025_04_21_230944_create_requests_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('requests', function (Blueprint $table) {
             $table->id('id');
-            $table->string('status')->default('En attente');
+            $table->string('status')->default('EN ATTENTE');
             $table->string('filename');
             $table->string('requestor_id');
             $table->string('pdf_path');
-            $table->text('comment')->nullable();
+            $table->text('secretary_comment')->nullable();
+            $table->text('director_comment')->nullable();
             $table->timestamps(); // This creates both created_at and updated_at columns
         });
     }
