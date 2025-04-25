5aceda46aea5516cea8e9fc64c89733b2af1931a
Santiago-Soto-21 <santisotofajardo@gmail.com>
2025-04-25 18:15:18 -0400

    Fix pdf title

--- resources/views/pdf/template.blade.php
+++ resources/views/pdf/template.blade.php
@@ -2,8 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>UQO - DII - Plan du cours INF1563</title>
-    <meta name="author" content="Baaziz, Nadia" />
+    <title>UQO - Plan du cours {!! $data['sigle'] !!}</title>
     <style>
         @font-face {
           font-family: 'verdana';
